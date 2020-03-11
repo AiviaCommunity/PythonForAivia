d73b541f0e22499a3417920bf0f07935f8e717bd
Trevor Lancon <37873827+tlancon@users.noreply.github.com>
2020-03-11 04:53:36 -0500

    Added MaxIP and MinIP scripts

--- Recipes/MaxSlices.py
+++ Recipes/MaxSlices.py
@@ -11,6 +11,25 @@ voxel in XY will be replaced with the maximum value found within the 2
 slices before and after that slice.
 
 Works only in 3D.
+
+Requirements
+------------
+numpy
+scikit-image
+
+Parameters
+----------
+Input Image : Aivia channel
+    Input channel to use for the transform.
+
+Width : int
+    Number of slices around the current slice to consider for the MIP.
+
+Returns
+-------
+Aivia channel
+    Result of the transform
+
 """
 
 # [INPUT Name:inputImagePath Type:string DisplayName:'Input Image']
@@ -20,24 +39,34 @@ def run(params):
     image_location = params['inputImagePath']
     result_location = params['resultPath']
     width = int(params['width'])
+    tCount = int(params['TCount'])
     if not os.path.exists(image_location):
-        print(f'Error: {image_location} does not exist')
+        print(f"Error: {image_location} does not exist")
         return;
         
     image_data = imread(image_location)
-    print(f'Input data shape : {image_data.shape}')
-    
-    if len(image_data.shape) == 2:
-        print(f'Error: Maximum intensity projection cannot be applied to 2D images.')
-        return;
-    
+    dims = image_data.shape
     output_data = np.empty_like(image_data)
     
-    width_eff = int(width/2)
-    max_slice = int(image_data.shape[0])
-    for i in np.arange(0,max_slice):
-        output_data[i,:,:] = np.amax(image_data[i-min(width_eff,i):i+min(width_eff,max_slice-i),:,:], axis=0)
-
+    if len(dims) == 2 or (len(dims) == 3 and tCount > 1):
+        print('Error: Maximum intensity projection cannot be applied to 2D images.')
+        return;
+    
+    if tCount == 1:
+        max_slice = int(dims[0])
+        width_eff = min(int(width/2), max_slice)
+        for i in np.arange(0, max_slice):
+            output_data[i, :, :] = np.amax(
+                image_data[i-min(width_eff,i):i+min(width_eff,max_slice-i), :, :], axis=0
+            )
+    else:
+        max_slice = int(dims[1])
+        width_eff = min(int(width/2), max_slice)
+        for i in np.arange(0, max_slice):
+            output_data[:, i, :, :] = np.amax(
+                image_data[:, i-min(width_eff,i):i+min(width_eff,max_slice-i), :, :], axis=1
+            )
+            
     imsave(result_location, output_data)
 
 
@@ -48,8 +77,3 @@ if __name__ == '__main__':
     params['width'] = 2;
     
     run(params)
-
-
-# CHANGELOG
-# v1.00 TL - Original script by Trevor Lancon (trevorl@drvtechnologies.com)
-#
