ec8f7f9a085c5cd36f7b12cf3f13c9a3c4edf505
seismonastic <hungyu.chang@aivia-software.com>
2020-12-23 10:14:53 -0800

    Rename venv recipes folder Rename Cellpose_venv to CellposeVenv/ Rename StarDist_venv to StardistVenv/ Add zipped venv subfolders to ZippedVenvFolders

---
+++ VenvRecipes/CellposeVenv/CellposeForAivia.py
@@ -0,0 +1,93 @@
+import os.path
+import subprocess
+import pathlib
+from pathlib import Path
+import venv
+
+"""
+This Aivia python recipe invokes the subprocess to execute Cellpose_venv.py
+under the required virtual environment. During the first execution, it will
+create the required virtual environment.
+"""
+
+# [INPUT Name:inputImagePath Type:string DisplayName:'Input Image']
+# [INPUT Name:diameter Type:double DisplayName:'Diameter (px)' Default:30.0 Min:0.0 Max:1000.0]
+# [INPUT Name:modelType Type:int DisplayName:'Model Type (0=cyto, 1=nuc)' Default:0 Min:0 Max:1]
+# [INPUT Name:cellThreshold Type:double DisplayName:'Cell Threshold' Default:0.0 Min:0.0 Max:6.0]
+# [INPUT Name:flowThreshold Type:double DisplayName:'Flow Threshold' Default:0.4 Min:0.0 Max:1.0]
+# [OUTPUT Name:confMapPath Type:string DisplayName:'Confidence Map']
+# [OUTPUT Name:maskPath Type:string DisplayName:'Mask']
+def run(params):
+
+    env_dir = pathlib.Path(os.path.dirname(os.path.realpath(__file__))) / 'env'
+
+    if not os.path.exists(env_dir):
+        env_dir.mkdir(parents=False, exist_ok=True)
+        venv.create(env_dir, system_site_packages=True)
+
+        exec_path = env_dir / 'Scripts' / 'python.exe'
+
+        requirement_dir = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
+        file1 = open(requirement_dir / 'requirements.txt', 'r')
+        Lines = file1.readlines()
+        Lines = {l.strip() for l in Lines}
+
+        subprocess.check_call(
+            [str(exec_path), '-m', 'pip', 'install', *Lines])
+
+    # Check if input image exists
+    inputImagePath_ = params['inputImagePath']
+    if not os.path.exists(inputImagePath_):
+        raise ValueError('Error: {inputImagePath_} does not exist')
+
+    # Get Z count and T count
+    z_count, t_count = [int(params[f'{s}Count']) for s in ['Z', 'T']]
+
+    # Get the path of the folder that contains this python script
+    parentFolder = str(Path(__file__).parent)
+
+    # Get the path of python executable in the virtual environment
+    pythonExec_ = parentFolder + '\\env\\Scripts\\python.exe'
+
+    # Get the path of the python script to run under the virtual environment
+    scrptPath_ = parentFolder + '\\Data\\Cellpose_venv.py'
+
+    # Get input, output, and parameters as strings
+    zCount_ = str(z_count)
+    tCount_ = str(t_count)
+    diameter_ = params['diameter']
+    model_type_ = params['modelType']
+    conf_map_path_ = params['confMapPath']
+    mask_path_ = params['maskPath']
+    cellprob_threshold_ = params['cellThreshold']
+    flow_threshold_ = params['flowThreshold']
+
+    # Display input, output, and parameters
+    print('------------------------------------------')
+    print('         Cellpose Python Recipe')
+    print('------------------------------------------')
+    print(f'        pythonExec_= {pythonExec_}')
+    print(f'         scrptPath_= {scrptPath_}')
+    print(f'    inputImagePath_= {inputImagePath_}')
+    print(f'            zCount_= {zCount_}')
+    print(f'            tCount_= {tCount_}')
+    print(f'          diameter_= {diameter_}')
+    print(f'        model_type_= {model_type_}')
+    print(f'     conf_map_path_= {conf_map_path_}')
+    print(f'         mask_path_= {mask_path_}')
+    print(f'cellprob_threshold_= {cellprob_threshold_}')
+    print(f'    flow_threshold_= {flow_threshold_}')
+    print('------------------------------------------')
+
+    # Run the script under the virtual environment
+    proc = subprocess.Popen(
+                [pythonExec_, scrptPath_, inputImagePath_, zCount_, tCount_,
+                 diameter_, model_type_, conf_map_path_, mask_path_,
+                 cellprob_threshold_, flow_threshold_],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT)
+
+    # Write sub process outputs
+    for line in proc.stdout:
+        print(line.rstrip())
+    proc.wait()
--- VenvRecipes/README.md
+++ VenvRecipes/README.md
@@ -6,7 +6,7 @@ Each of these recipes will create the required virtual environment for itself at
 
 ## How to download VenvRecipes
 
-1. Please download the whole folder of the recipe that you want to use. Although GitHub does not support subfolder download, you can still use website like [DownGit](https://minhaskamal.github.io/DownGit/#/home) to download the subfolder.
+1. Please download the whole folder of the recipe that you want to use. Since GitHub does not support subfolder download, we put zipped folders here for you: [ZippedVenvFolders](ZippedVenvFolders).
 
 2. Find the `README.md` file in the subfolder to see the details of each recipe.
 
@@ -14,5 +14,5 @@ Each of these recipes will create the required virtual environment for itself at
 
 Segmentation:
 
-1. [Cellpose_venv](./Cellpose_venv)
-2. [StarDist_venv](./StarDist_venv)
+1. [CellposeVenv](./CellposeVenv)
+2. [StarDistVenv](./StarDistVenv)
