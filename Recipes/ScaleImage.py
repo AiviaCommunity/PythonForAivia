169586d943b6f9c42f2eaf63dbeee2f19d2173ff
Patrice Mascalchi <18047969+pmascalchi@users.noreply.github.com>
2021-11-25 16:00:32 +0100

    Removed Tkinter GUI to fallback on Aivia recipe UI
    Version 1.11

--- Recipes/ScaleImage.py
+++ Recipes/ScaleImage.py
@@ -5,14 +5,12 @@ import imagecodecs
 import numpy as np
 from tifffile import imread, imwrite
 from skimage import transform, img_as_uint, img_as_ubyte
-import tkinter as tk
 
-aivia_version = 'Aivia 10.0.0'
+aivia_version = 'Aivia 10.0.1'
 
 """
 Scales the input channel up or down (isotropic factor). Option for interpolation is in the code.
 Works only for 2D/3D (not timelapses) and for single channels.
-IMPORTANT: This scripts works for Aivia 10 or more.
 
 Documentation
 -------------
@@ -22,7 +20,6 @@ Requirements
 ------------
 numpy (comes with Aivia installer)
 scikit-image (comes with Aivia installer)
-tkinter (comes with Aivia installer, but in 10.0.0, needs manual install with 'pip install tk')
 imagecodecs (comes with Aivia installer)
 tifffile (comes with Aivia installer)
 
@@ -40,9 +37,6 @@ New image:
     Opens Aivia to display the new scaled image.
 
 """
-
-default_scale_factor_XY = 2
-default_scale_factor_Z = 1
 interpolation_mode = 1  # 0: Nearest-neighbor, 1: Bi-linear , 2: Bi-quadratic, 3: Bi-cubic, 4: Bi-quartic, 5: Bi-quintic
 
 # automatic parameters
@@ -50,10 +44,16 @@ aivia_path = 'C:\\Program Files\\Leica Microsystems\\' + aivia_version + '\\Aivi
 
 
 # [INPUT Name:inputImagePath Type:string DisplayName:'Input Channel']
+# [INPUT Name:scaleDirection Type:int DisplayName:'Down or Upscale (0 or 1)' Default:0 Min:0 Max:1]
+# [INPUT Name:scaleFactorZ Type:double DisplayName:'Z scale factor' Default:1.0 Min:0.01 Max:20.0]
+# [INPUT Name:scaleFactorXY Type:double DisplayName:'XY scale factor' Default:1.0 Min:0.01 Max:20.0]
 # [OUTPUT Name:resultPath Type:string DisplayName:'Duplicate of input']
 def run(params):
     image_location = params['inputImagePath']
     result_location = params['resultPath']
+    scale_factor_xy = float(params['scaleFactorXY'])
+    scale_factor_z = float(params['scaleFactorZ'])
+    scale_direction = params['scaleDirection']
     zCount = int(params['ZCount'])
     tCount = int(params['TCount'])
     if not os.path.exists(image_location):
@@ -73,102 +73,39 @@ def run(params):
         print('Error: Cannot handle timelapses yet.')
         return
 
-    output_data = np.empty_like(image_data)
+    # output_data = np.empty_like(image_data)
 
-    # ------------------- defining a class for the GUI ----------------------------
-    class GUI:
-        def __init__(self, master, def_scalef_xy, def_scalef_z, img_data, res_path, interpol):
-            self.master = master
-            self.pane = tk.Frame(master)
+    if scale_direction == 0:
+        scale_factor_xy = 1 / scale_factor_xy
+        scale_factor_z = 1 / scale_factor_z
 
-            master.title('Scale image')
-            self.tmp_path = ''
-            self.scale_order = interpol
+    # Defining axes for output metadata and scale factor variable
+    final_scale = None
+    if tCount == 1 and zCount > 1:         # 3D
+        axes = 'YXZ'
+        final_scale = (scale_factor_z, scale_factor_xy, scale_factor_xy)
 
-            self.scale_direction = tk.StringVar()
-            self.scale_dir_option1 = tk.Radiobutton(master, text='Upscale', variable=self.scale_direction, value='up')
-            self.scale_dir_option1.grid(row=1, column=1, stick=tk.W)
-            self.scale_dir_option2 = tk.Radiobutton(master, text='Downscale', variable=self.scale_direction,
-                                                    value='down')
-            self.scale_dir_option2.grid(row=1, column=2, stick=tk.W)
-            self.scale_dir_option2.select()
+    elif tCount == 1 and zCount == 1:      # 2D
+        axes = 'YX'
+        final_scale = scale_factor_xy
 
-            self.scale_factor_xy_lbl = tk.Label(master, text='Scaling Factor XY: ')
-            self.scale_factor_xy_lbl.grid(row=2, column=1, stick=tk.W)
-            self.scale_factor_xy = tk.StringVar()
-            self.scale_factor_xy.set(def_scalef_xy)
-            self.scale_fact_option = tk.Entry(master, textvariable=self.scale_factor_xy, width=10)
-            self.scale_fact_option.grid(row=2, column=2, stick=tk.W)
+    scaled_img = transform.rescale(image_data, final_scale, interpolation_mode)
 
-            self.scale_factor_z_lbl = tk.Label(master, text='Scaling Factor Z: ')
-            self.scale_factor_z_lbl.grid(row=3, column=1, stick=tk.W)
-            self.scale_factor_z = tk.StringVar()
-            self.scale_factor_z.set(def_scalef_z)
-            self.scale_fact_option = tk.Entry(master, textvariable=self.scale_factor_z, width=10)
-            self.scale_fact_option.grid(row=3, column=2, stick=tk.W)
+    # Formatting result array
+    if image_data.dtype is np.dtype('u2'):
+        out_data = img_as_uint(scaled_img)
+    else:
+        out_data = img_as_ubyte(scaled_img)
 
-            self.process_lbl = tk.Label(master, text='')
-            self.process_lbl.grid(row=4, column=1, stick=tk.W)
-            self.process_button = tk.Button(master, text='Process', command=lambda: self.process_frames(img_data, res_path))
-            self.process_button.grid(row=4, column=4, sticky=tk.E, pady=5)
-
-        def process_frames(self, img_data, res_path):
-            """
-            Performs transform depending on input parameters
-            """
-            self.process_lbl['text'] = '... processing ...'
-            self.pane.update_idletasks()
-
-            scale_factor_xy = float(self.scale_factor_xy.get())
-            scale_factor_z = float(self.scale_factor_z.get())
-            if self.scale_direction.get() == 'down':
-                scale_factor_xy = 1 / scale_factor_xy
-                scale_factor_z = 1 / scale_factor_z
-
-            # Defining axes for output metadata and scale factor variable
-            final_scale = None
-            if tCount == 1 and zCount > 1:         # 3D
-                axes = 'YXZ'
-                final_scale = (scale_factor_z, scale_factor_xy, scale_factor_xy)
-
-            elif tCount == 1 and zCount == 1:      # 2D
-                axes = 'YX'
-                final_scale = scale_factor_xy
-
-            scaled_img = transform.rescale(img_data, final_scale, self.scale_order)
-
-            # Formatting result array
-            if img_data.dtype is np.dtype('u2'):
-                out_data = img_as_uint(scaled_img)
-            else:
-                out_data = img_as_ubyte(scaled_img)
-
-            self.process_lbl['text'] = '... processed ...'
-            self.pane.update_idletasks()
-
-            self.tmp_path = res_path.replace('.tif', '-tmp.tif')
-            imwrite(self.tmp_path, out_data, photometric='minisblack', metadata={'axes': axes})
-
-            self.master.quit()
-
-    # -------------------- run GUI ---------------------------
-    root_ps = tk.Tk()
-    root_ps.configure(bg='#232121')
-    root_ps.option_add('*HighlightThickness', 0)
-    root_ps.option_add('*Background', '#232121')
-    root_ps.option_add('*Foreground', '#F1E7E3')
-    root_ps.option_add('*Font', '{MS Sans Serif}')
-    runTransform = GUI(root_ps, default_scale_factor_XY, default_scale_factor_Z, image_data, result_location,
-                       interpolation_mode)
-    root_ps.mainloop()
+    tmp_path = result_location.replace('.tif', '-tmp.tif')
+    imwrite(tmp_path, out_data, photometric='minisblack', metadata={'axes': axes})
 
     # Dummy save
     # imwrite(result_location, output_data)
 
     # Run external program
-    temp_location = runTransform.tmp_path
-    cmdLine = 'start \"\" \"' + aivia_path + '\" \"' + temp_location + '\"'
-    # cmdLine = 'start \"\" \"' + IJ_path + '\" \"' + temp_location + '\"'
+    cmdLine = 'start \"\" \"' + aivia_path + '\" \"' + tmp_path + '\"'
+    # cmdLine = 'start \"\" \"' + IJ_path + '\" \"' + tmp_path + '\"'
 
     args = shlex.split(cmdLine)
     subprocess.run(args, shell=True)
@@ -179,9 +116,9 @@ if __name__ == '__main__':
               'resultPath': 'D:\\python-tests\\scaled.tif',
               'TCount': 1,
               'ZCount': 51}
-
     run(params)
 
 # CHANGELOG
 # v1_00: - scaling in XYZ with same factor
 # v1_10: - scaling in XY and Z are now independent
+# v1_11: - tkinter not installed by default so removing all code and adding parameters in Aivia UI
