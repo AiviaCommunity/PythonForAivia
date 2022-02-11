2343f2410f163bef866cd19b8a02f6b020d97435
Patrice Mascalchi <18047969+pmascalchi@users.noreply.github.com>
2022-02-11 13:50:07 +0100

    Added a lot more color maps
    Needed some code adjustment as color maps do not all have the same structure

--- Recipes/ZColorCoding.py
+++ Recipes/ZColorCoding.py
@@ -1,3 +1,4 @@
+# import time
 from matplotlib import cm
 import numpy as np
 from skimage.io import imread, imsave
@@ -15,7 +16,8 @@ matlplotlib (comes with Aivia installer)
 Parameters
 ----------
 - Input channel
-- Colormap choice is hardcoded below (with index from 0 to 4)
+- Colormap choice (with index from 0 to ...)
+See maps here: https://matplotlib.org/stable/gallery/color/colormap_reference.html
 
 Returns
 ----------
@@ -27,16 +29,19 @@ Usage
 * In python: modify code to select a colormap and modify parameters at the end of code. Run the .py file.
 '''
 
-cmaps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis']
-selected_map = cmaps[2]
+cmaps = ['viridis', 'plasma', 'inferno', 'magma', 'cividis', 'spring', 'summer', 'autumn', 'winter', 'cool',
+         'Wistia', 'hot', 'afmhot', 'gist_heat', 'copper', 'hsv',
+         'gnuplot', 'gnuplot2', 'CMRmap', 'brg', 'gist_rainbow', 'rainbow', 'jet', 'turbo']
 
 
 # [INPUT Name:inputImagePath Type:string DisplayName:'Input Image']
+# [INPUT Name:colorMapChoice Type:int DisplayName:'Color map index (see python code)' Default:0 Min:0 Max:100]
 # [OUTPUT Name:resultPathBlue Type:string DisplayName:'Z Coloring - Blue']
 # [OUTPUT Name:resultPathGreen Type:string DisplayName:'Z Coloring - Green']
 # [OUTPUT Name:resultPathRed Type:string DisplayName:'Z Coloring - Red']
 def run(params):
     image_location = params['inputImagePath']
+    selected_map = cmaps[int(params['colorMapChoice'])]
     result_location_red = params['resultPathRed']
     result_location_green = params['resultPathGreen']
     result_location_blue = params['resultPathBlue']
@@ -51,18 +56,24 @@ def run(params):
     image_data = imread(image_location)
     input_dims = np.asarray(image_data.shape)
     print('-- Input dimensions (expected Z, Y, X or T, Z, Y, X): ', input_dims, ' --')
+    print('-- Selected color map is: ', selected_map)
 
     output_dims = np.insert(input_dims, 0, 3)
     output_data = np.zeros(output_dims).astype(image_data.dtype)
 
     # Get discrete values from colormap and apply
-    final_color_map = cm.get_cmap(selected_map, zCount)
+    color_map = cm.get_cmap(selected_map, zCount)
+    if hasattr(color_map, 'colors'):
+        final_colors = color_map.colors
+    else:
+        # Some colors have a different type > extracting color ranges is different
+        final_colors = color_map(np.linspace(0, 1, zCount))
 
     if tCount == 1 or zCount == 1:
         print('-- processing 3D dataset --')
         for z in range(input_dims[0]):
             # Collect color map for precise z-slice
-            current_col = final_color_map.colors[z, :3]
+            current_col = final_colors[z, :3]
 
             for c in range(3):
                 output_data[c, z, :, :] = image_data[z, :, :] * current_col[c]
@@ -72,9 +83,10 @@ def run(params):
         print('-- processing 4D dataset --')
         for z in range(input_dims[1]):
             # Collect color map for precise z-slice
-            current_col = final_color_map.colors[z, :3]
+            current_col = final_colors[z, :3]
 
             for t in range(input_dims[0]):
+                # print('Time: ', time.ctime(time.time()), ' T: ', t, ' Z: ', z)
                 for c in range(3):
                     output_data[c, t, z, :, :] = image_data[t, z, :, :] * current_col[c]
 
@@ -84,14 +96,11 @@ def run(params):
 
 
 if __name__ == '__main__':
-    params = {'inputImagePath':
-                  'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0_3D-TL.aivia.tif'
-        , 'resultPathRed':
-                  'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0.aivia-R.tif'
-        , 'resultPathGreen':
-                  'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0.aivia-G.tif'
-        , 'resultPathBlue':
-                  'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0.aivia-B.tif'
-        , 'TCount': '3', 'ZCount': '100'}
+    params = {'inputImagePath': 'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0_3D-TL.aivia.tif',
+              'colorMapChoice': 5,
+              'resultPathRed': 'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0.aivia-R.tif',
+              'resultPathGreen': 'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0.aivia-G.tif',
+              'resultPathBlue': 'D:\\AIVIA working directory\\_Tests\\neuronimage_Crop_ML_resized_9.0.aivia-B.tif',
+              'TCount': '3', 'ZCount': '100'}
 
     run(params)
