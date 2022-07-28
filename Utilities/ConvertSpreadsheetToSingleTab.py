2925132febe46b46b9abd48c8b8cc6f7873cfc74
Patrice Mascalchi <18047969+pmascalchi@users.noreply.github.com>
2022-07-28 18:12:27 +0200

    Virtual environment activation within script

--- Utilities/ConvertSpreadsheetToSingleTab.py
+++ Utilities/ConvertSpreadsheetToSingleTab.py
@@ -1,6 +1,25 @@
+# -------- Activate virtual environment -------------------------
 import os
-import pandas as pd
 import ctypes
+import sys
+from pathlib import Path
+parentFolder = str(Path(__file__).parent.parent)
+activate_path = parentFolder + '\\env\\Scripts\\activate_this.py'
+if os.path.exists(activate_path):
+    exec(open(activate_path).read(), {'__file__': activate_path})
+    print(f'Aivia virtual environment activated\nUsing python: {activate_path}')
+else:
+    # Attempt to still run the script with main Aivia python interpreter
+    error_mess = f'Error: {activate_path} was not found.\nPlease run the \'FirstTimeSetup.py\' script in Aivia first.'
+    ans = ctypes.windll.user32.MessageBoxW(0, error_mess, 'Error', 1)
+    if ans == 2:
+        sys.exit(error_mess)
+    print('\n'.join(['#' * 40, error_mess,
+                     'Now trying to fallback on python environment specified in Aivia options > Advanced.',
+                     '#' * 40]))
+# ---------------------------------------------------------------
+
+import pandas as pd
 import wx
 import threading
 
@@ -17,9 +36,9 @@ The converted file will be saved with the same name as the original but with
 
 Requirements
 ------------
-pandas      (comes with Aivia installer)
-openpyxl    (comes with Aivia installer)
-xlrd        (comes with Aivia installer)
+pandas
+openpyxl
+xlrd
 wxPython
 
 (openpyxl and xlrd are Pandas requirements, but are not always
