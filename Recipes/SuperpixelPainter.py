2da1815111062139fd1b54e8eb8f964e2b96c9fb
Patrice Mascalchi <18047969+pmascalchi@users.noreply.github.com>
2022-11-28 16:03:14 +0100

    Fixed a bug with source image in wx UI
    Also cleaned the code to be PEP8

--- Recipes/SuperpixelPainter.py
+++ Recipes/SuperpixelPainter.py
@@ -5,6 +5,7 @@ import sys
 from pathlib import Path
 parentFolder = str(Path(__file__).parent.parent)
 activate_path = parentFolder + '\\env\\Scripts\\activate_this.py'
+
 if os.path.exists(activate_path):
     exec(open(activate_path).read(), {'__file__': activate_path})
     print(f'Aivia virtual environment activated\nUsing python: {activate_path}')
@@ -51,12 +52,6 @@ scikit-image
 PIL
 wxPython
 
-For Aivia 10.x with embedded python, the guide to install non-standard packages (windows and macOS):
-
-* Open terminal
-* Change directory: cd "path/to/Aivia/Python/directory/" (For e.g. C:\Program Files\Leica Microsystems\Aivia 10.5.0\Python)
-* Run: python -m pip install <name_of_the_module>
-
 Parameters
 ----------
 Input Image : Aivia channel
@@ -85,19 +80,17 @@ def run(params):
     print('read')
     if not os.path.exists(image_location):
         print(f'Error: {image_location} does not exist')
-        return;
+        return
         
     if zCount > 1 or tCount > 1:
         print('Currently this only supports 2D images with no time dimension.')
         return
     
-    
-    image_data=imread(image_location)
+    image_data = imread(image_location)
     mask_data = paint_superpixels(image_location)
     
-    
     mask_data = resize(mask_data, image_data.shape, anti_aliasing=False)
-    mask_data=img_as_ubyte(mask_data)
+    mask_data = img_as_ubyte(mask_data)
     
     imsave(result_mask_location, mask_data)
     imsave(result_object_location, mask_data)
@@ -121,13 +114,10 @@ def paint_superpixels(image_location):
     (N, M) array
         Binary mask based on the user's painting.
     """
-    
-
     app = wx.App()    
     frame = MyFrame(image_location)
     app.MainLoop()
 
-    
     return frame.mask[:, :, 0]
     
 
@@ -157,22 +147,19 @@ class MyFrame(wx.Frame):
     """
         
     def __init__(self, image_location):
-        super().__init__(parent=None, title='Superpixel Painter', size = wx.Size( 800,800 ))
+        super().__init__(parent=None, title='Superpixel Painter', size=wx.Size(800, 800))
 
-
-       
-        image_np=imread(image_location)
+        image_np = imread(image_location)
         frame_size = self.GetSize()
-        frame_h = (frame_size[0]) *0.7
-        frame_w = (frame_size[1]) *0.7
-        image_np_sc = resize(image_np, (frame_h,frame_w), anti_aliasing=False)
+        frame_h = (frame_size[0]) * 0.7
+        frame_w = (frame_size[1]) * 0.7
+        image_np_sc = resize(image_np, (frame_h, frame_w), anti_aliasing=False)
         self.W = image_np_sc.shape[0]
         self.H = image_np_sc.shape[1]
 
         image = wx.Image(self.W, self.H)
-        image.SetData(image_np.tobytes())
+        image.SetData(image_np_sc.tobytes())
         wxBitmap = image.ConvertToBitmap()
-        
         
         max_markers = int(0.01 * (self.W*self.H))
         self.marker_color = [1.00, 0.73, 0.03]
@@ -180,44 +167,41 @@ class MyFrame(wx.Frame):
         self.panel = wx.Panel(self)
         self.panel.SetBackgroundColour("gray")
 
-        
         self.mainSizer = wx.BoxSizer(wx.VERTICAL)
         
         user_instructions = 'Click left mouse button to paint superpixels. Click right mouse button to erase. ' \
                             'Middle click to fill a contour. Close the app when finished.'
         self.instruction = wx.StaticText(self.panel, label=user_instructions)
-        self.instruction.SetForegroundColour(wx.Colour(255,255,255))        
-        self.mainSizer.Add(self.instruction, 0,wx.ALL | wx.CENTER, 5)
+        self.instruction.SetForegroundColour(wx.Colour(255, 255, 255))
+        self.mainSizer.Add(self.instruction, 0, wx.ALL | wx.CENTER, 5)
         
         self.imageCtrl = wx.StaticBitmap(self.panel, wx.ID_ANY, 
                                          wxBitmap)
                 
         self.mainSizer.Add(self.imageCtrl, 0, wx.ALL, 5)        
-        self.imageCtrl.SetBitmap(wxBitmap)       
-        
+        self.imageCtrl.SetBitmap(wxBitmap)
         
         self.marker = wx.StaticText(self.panel, label='Marker')
-        self.marker.SetForegroundColour(wx.Colour(255,255,255))
-        self.mainSizer.Add(self.marker, 0, wx.ALL , 5)
+        self.marker.SetForegroundColour(wx.Colour(255, 255, 255))
+        self.mainSizer.Add(self.marker, 0, wx.ALL, 5)
         
-        self.marker_sld = wx.Slider(self.panel, value = 1, minValue = 10, maxValue = max_markers,
-        style = wx.SL_HORIZONTAL|wx.SL_LABELS)
-        self.mainSizer.Add(self.marker_sld,1,flag = wx.EXPAND , border = 20)
+        self.marker_sld = wx.Slider(self.panel, value=1, minValue=10, maxValue=max_markers,
+                                    style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.mainSizer.Add(self.marker_sld, 1, flag=wx.EXPAND, border=20)
 
         self.compactness = wx.StaticText(self.panel, label='Compactness')
-        self.compactness.SetForegroundColour(wx.Colour(255,255,255))
-        self.mainSizer.Add(self.compactness, 0, wx.ALL , 5)
-        self.compactness_float=0.001
+        self.compactness.SetForegroundColour(wx.Colour(255, 255, 255))
+        self.mainSizer.Add(self.compactness, 0, wx.ALL, 5)
+        self.compactness_float = 0.001
 
-
-        self.compactness_sld = wx.Slider(self.panel, value = 1, minValue = 1, maxValue = 10,
-        style = wx.SL_HORIZONTAL|wx.SL_LABELS)
-        self.mainSizer.Add(self.compactness_sld,1,flag = wx.EXPAND , border = 20) 
+        self.compactness_sld = wx.Slider(self.panel, value=1, minValue=1, maxValue=10,
+                                         style=wx.SL_HORIZONTAL|wx.SL_LABELS)
+        self.mainSizer.Add(self.compactness_sld, 1, flag=wx.EXPAND, border=20)
 
         self.clear_button = wx.Button(self.panel, wx.ID_ANY, 'Clear', (10, 10))
-        self.mainSizer.Add(self.clear_button, 0, wx.ALL , 5)
+        self.mainSizer.Add(self.clear_button, 0, wx.ALL, 5)
         
-        self.input_image_np=image_np_sc
+        self.input_image_np = image_np_sc
         self.superpixels = watershed(sobel(self.input_image_np), markers=self.marker_sld.GetValue(), 
                                      compactness=self.compactness_sld.GetValue()*self.compactness_float)
         self.boundaries = img_as_ubyte(mark_boundaries(self.input_image_np, self.superpixels, color=self.marker_color))
@@ -240,10 +224,8 @@ class MyFrame(wx.Frame):
     def wximage_to_numpy(self, image):
 
         arr = np.asarray(image.GetDataBuffer())
-        image_np = np.copy(np.reshape(arr, (image.GetWidth(), image.GetHeight(),3)))
+        image_np = np.copy(np.reshape(arr, (image.GetWidth(), image.GetHeight(), 3)))
         return image_np
-        
-           
 
     def clear_mask(self, event):
         """
@@ -258,9 +240,8 @@ class MyFrame(wx.Frame):
         Updates the app's displayed image.
         """
         
-        wxBitmap= wx.Image(self.W,self.H, self.display_image)
+        wxBitmap = wx.Image(self.W, self.H, self.display_image)
         self.imageCtrl.SetBitmap(wx.Bitmap(wxBitmap))
-        
 
     def update_superpixels(self, event):
         """
@@ -304,20 +285,19 @@ class MyFrame(wx.Frame):
         
         for i in range(3):
             self.mask[:, :, i] = flood_fill(self.mask[:, :, i], seed_point=(event.y, event.x), new_value=255,
-                                                tolerance=1)
+                                            tolerance=1)
             self.display_image[:, :, i] = np.where(self.mask[:, :, i] == 255, 255, self.display_image[:, :, i])
         self.update_image()
-        
 
-
-    
-            
 
 if __name__ == '__main__':
     params = {}
-    params['inputImagePath'] = '2D_PigSkin.tif'
-    params['resultMaskPath'] = '2D_PigSkinMask.tif'
-    params['resultObjectPath'] = '2D_PigSkinResult.tif'
-    params['TCount']=1
-    params['ZCount']=1
+    params['inputImagePath'] = 'test_8b.tif'
+    params['resultMaskPath'] = 'test_8b_res.tif'
+    params['resultObjectPath'] = 'test_8b_objres.tif'
+    params['TCount'] = 1
+    params['ZCount'] = 1
     run(params)
+
+# CHANGELOG:
+# v1.01: - Bug fixed with wxPython.Image.SetData (ValueError: Invalid data buffer size.) >> was not using rescaled img.
