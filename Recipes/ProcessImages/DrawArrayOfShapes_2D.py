78551c03692f530084b1e92b134f0d35927f2225
Patrice Mascalchi <18047969+pmascalchi@users.noreply.github.com>
2023-07-03 14:44:57 +0200

    Update DrawArrayOfShapes_2D.py

--- Recipes/ProcessImages/DrawArrayOfShapes_2D.py
+++ Recipes/ProcessImages/DrawArrayOfShapes_2D.py
@@ -4,16 +4,21 @@ import ctypes
 import sys
 from pathlib import Path
 
-parentFolder = str(Path(__file__).parent.parent.parent)
-activate_path = parentFolder + '\\env\\Scripts\\activate_this.py'
+def search_activation_path():
+    for i in range(5):
+        final_path = str(Path(__file__).parents[i]) + '\\env\\Scripts\\activate_this.py'
+        if os.path.exists(final_path):
+            return final_path
+    return ''
 
+activate_path = search_activation_path()
 if os.path.exists(activate_path):
     exec(open(activate_path).read(), {'__file__': activate_path})
     print(f'Aivia virtual environment activated\nUsing python: {activate_path}')
 else:
     error_mess = f'Error: {activate_path} was not found.\n\nPlease check that:\n' \
                  f'   1/ The \'FirstTimeSetup.py\' script was already run in Aivia,\n' \
-                 f'   2/ The current python recipe is in the "\\PythonVenvForAivia\\Recipes" subfolder.'
+                 f'   2/ The current python recipe is in one of the "\\PythonEnvForAivia\\" subfolders.'
     ctypes.windll.user32.MessageBoxW(0, error_mess, 'Error', 0)
     sys.exit(error_mess)
 # ---------------------------------------------------------------
@@ -253,3 +258,4 @@ if __name__ == '__main__':
 
 # CHANGELOG
 # v1.00: - using Skimage.draw shapes. Adding virtual env for GUI
+# v1.01: - New virtual env code for auto-activation
