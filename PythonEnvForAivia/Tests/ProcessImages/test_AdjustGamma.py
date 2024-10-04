9a52f9ac4c29f42c21c53473eea4d38f9a880565
Gordon Sun <gsun1729@gmail.com>
2024-10-04 11:38:07 -0700

    mv'd tests for ProcessImages into their own ProcessImages dir, along with their configuration files and ground truth directories to reduce overcrowding in one dir for future test cases being added
    restructured project such that individual tests are located in their respective folder for tests of a given category.

---
+++ PythonEnvForAivia/Tests/ProcessImages/test_AdjustGamma.py
@@ -0,0 +1,35 @@
+import unittest
+import json
+import os
+from Recipes.ProcessImages import AdjustGamma
+from Tests.utils.comparison import isIdentical
+
+
+def run_test(config):
+    ground_truth_path = config.pop('groundTruthPath')
+    AdjustGamma.run(params=config)
+    assert isIdentical(ground_truth_path, config.get("resultPath"))
+    return True
+
+class Test_AdjustGamma(unittest.TestCase):
+    def dynamic_test_generator(self, config):
+        self.assertTrue(run_test(config))
+
+def generate_test_method(config):
+    def test_method(self):
+        self.dynamic_test_generator(config)
+    return test_method
+
+config_json_path = os.path.join(os.path.dirname(__file__), "TestConfigs","AdjustGamma_configs.json")
+with open(config_json_path) as f:
+    configurations = json.load(f)
+
+# Dynamically create test methods for each configuration
+for i, config in enumerate(configurations):
+    test_name = f"test_AdjustGamma_{i:02d}"  # Must start with "test_"
+    test_method = generate_test_method(config)
+    setattr(Test_AdjustGamma, test_name, test_method)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ PythonEnvForAivia/Tests/ProcessImages/test_SkeletonizeObjects.py
@@ -0,0 +1,39 @@
+import unittest
+import json
+import os
+from Recipes.ProcessImages import SkeletonizeObjects
+from Tests.utils.comparison import isIdentical
+
+
+# NOTE: This class does not work for RGB images, only works on 3D, 3D+T images
+
+def run_test(config):
+    ground_truth_path = config.pop('groundTruthPath')
+    ground_truth_objs_path = config.pop('groundTruthObjectsPath')
+    SkeletonizeObjects.run(params=config)
+    assert isIdentical(ground_truth_path, config.get("resultImagePath"))
+    assert isIdentical(ground_truth_objs_path, config.get("resultObjectPath"))
+    return True
+
+class Test_SkeletonizeObjects(unittest.TestCase):
+    def dynamic_test_generator(self, config):
+        self.assertTrue(run_test(config))
+
+def generate_test_method(config):
+    def test_method(self):
+        self.dynamic_test_generator(config)
+    return test_method
+
+config_json_path = os.path.join(os.path.dirname(__file__), "TestConfigs","SkeletonizeObjects_configs.json")
+with open(config_json_path) as f:
+    configurations = json.load(f)
+
+# Dynamically create test methods for each configuration
+for i, config in enumerate(configurations):
+    test_name = f"test_SkeletonizeObjects_{i:02d}"  # Must start with "test_"
+    test_method = generate_test_method(config)
+    setattr(Test_SkeletonizeObjects, test_name, test_method)
+
+
+if __name__ == "__main__":
+    unittest.main()
