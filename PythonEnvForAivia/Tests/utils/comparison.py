f7a93e0694a6b92b37079a5d11480793257871fc
Gordon Sun <gsun1729@gmail.com>
2024-10-07 13:31:53 -0700

    added comparison func for json outputs

--- PythonEnvForAivia/Tests/utils/comparison.py
+++ PythonEnvForAivia/Tests/utils/comparison.py
@@ -1,5 +1,6 @@
 import tifffile as tif
 import numpy as np
+import json
 
 
 def isIdentical(image_path1, image_path2):
@@ -20,3 +21,22 @@ def isIdentical(image_path1, image_path2):
         
         # Compare the arrays
         return np.array_equal(data1, data2)
+
+
+
+def sort_json(data):
+    if isinstance(data, dict):
+        return {k: sort_json(v) for k, v in sorted(data.items())}
+    elif isinstance(data, list):
+        return sorted(sort_json(x) for x in data)
+    else:
+        return data
+
+
+def isJsonIdentical(json_path1, json_path2):
+    with open(json_path1) as f1, open(json_path2) as f2:
+        json_data1 = json.load(f1)
+        json_data2 = json.load(f2)
+    if json_data1 == json_data2:
+        return True
+    return False
